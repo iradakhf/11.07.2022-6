09dfb83ee26a3883f0b0db7c1ac6abcd8cf291a2
iradakhf <iradakhf@code.edu.az>
2022-07-11 14:59:16 +0400

    add all

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ConsoleApp34
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            WordAdding wordAdding = new("Irada");
+            wordAdding.AddWord();
+        }
+
+        class WordAdding
+        {
+            public string Word { get; set; }
+            public WordAdding(string word)
+            {
+                this.Word = word;
+            }
+
+            public void AddWord()
+            {
+                Console.WriteLine($"Hello, {Word}, how are you doing");
+            }
+
+        }
+    }
+}
